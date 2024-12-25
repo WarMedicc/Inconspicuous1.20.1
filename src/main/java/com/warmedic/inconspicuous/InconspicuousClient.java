6be79a4a09c0798b87814f01bcb048d95142a739
WarMedic <aljaz.medic05@gmail.com>
2024-12-25 23:13:33 +0100

    Initial commit

---
+++ src/main/java/com/warmedic/inconspicuous/InconspicuousClient.java
@@ -0,0 +1,10 @@
+package com.warmedic.inconspicuous;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InconspicuousClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
